e2fcc7da7a3e0049976d8f1c80f138e025b5573e
きつねあざみ <kituneazami-mac@outlook.jp>
2025-04-23 21:44:40 +0900

    github action yaml の追加と調整

--- freeze.py
+++ freeze.py
@@ -4,6 +4,8 @@ from app import app
 freezer = Freezer(app)
 
 app.config['FREEZER_RELATIVE_URLS'] = True
+app.config['FREEZER_DESTINATION'] = './docs'
+
 
 if __name__ == '__main__':
     freezer.freeze()
