5501ed4f63ac31df6c412f8e9425a855b316e364
きつねあざみ <kituneazami-mac@outlook.jp>
2025-05-25 11:50:44 +0900

    Aboutを段落ごとにフェードインしながら表示されるように

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import generateStars from "./generate_stars.js";
 import fadeHeader from "./fade_header.js";
+import fadeinAbout from './fadein_about.js';
 
 generateStars()
 fadeHeader()
-
+fadeinAbout();
