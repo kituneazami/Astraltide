1ba558d9799c95d36fa59102c9ddec9d964e8718
きつねあざみ <kituneazami-mac@outlook.jp>
2025-05-25 11:50:44 +0900

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import generateStars from "./generate_stars.js";
 import fadeHeader from "./fade_header.js";
+import decorateAbout from "./decorate_about.js";
 
 generateStars()
 fadeHeader()
-
+decorateAbout()
